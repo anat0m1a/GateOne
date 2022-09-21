1cc3656f87f5f9de6200aeb02a7e52329cfde3fd
ItsThePlatypus <61837386+ItsThePlatypus@users.noreply.github.com>
2022-09-21 10:13:52 +0100

    Uploaded completed version of One-Time pad code

--- modules/tests/oneTime-TEST.c
+++ modules/tests/oneTime-TEST.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 void crypt(char *enc, char *key, char *sel);
@@ -11,6 +12,8 @@ int main(int argc, char *argv[])
         errorHandler();
 
     char *enc = argv[1], *key = argv[2], *sel = argv[3];
+    if(strlen(enc) != strlen(key))
+        errorHandler();
 
     if(*sel == '1' || *sel == '2')
         crypt(enc, key, sel);
@@ -22,7 +25,31 @@ int main(int argc, char *argv[])
 
 void crypt(char *enc, char *key, char *sel)
 {
-    // THIS NEEDS COMPLETING
+    static int arrSize;
+    arrSize = strlen(enc);
+    char outArr[arrSize];
+    
+    for(int i = 0; i < arrSize; i++)
+    {
+        enc[i] = toupper(enc[i]);
+        key[i] = toupper(key[i]);
+
+        if(*sel == '1')
+            outArr[i] = ((((((int)enc[i]) - 65) + (((int)key[i]) - 65)) % 26) + 65);
+        else
+        {
+            int tmp = (((int)enc[i]) - 65) - (((int)key[i]) - 65);
+            if(tmp < 0)
+                outArr[i] = (char)((26 - abs(tmp)) + 65);
+            else
+                outArr[i] = (char)((tmp % 26) + 65);
+        }
+    }
+
+    for(int i = 0; i < arrSize; i++)
+        putchar(outArr[i]);
+    putchar('\n');
+
     return;
 }
 
