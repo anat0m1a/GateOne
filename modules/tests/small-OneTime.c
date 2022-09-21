d94092f94d44ed9ad7d2cc94ecf9aebcb54b8152
ItsThePlatypus <61837386+ItsThePlatypus@users.noreply.github.com>
2022-09-21 11:35:40 +0100

    Changed to just decoder function
    To call:
    
    decrypt(<encoded string array>, <key from user input>)

--- modules/tests/small-OneTime.c
+++ modules/tests/small-OneTime.c
@@ -2,33 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-void crypt(char *enc, char *key, char *sel);
-void errorHandler();
-int main(int argc, char *argv[])
+void decrypt(char *enc, char *key)
 {
-    if(argc != 4){errorHandler()};
-    char *enc = argv[1], *key = argv[2], *sel = argv[3];
-    if(strlen(enc) != strlen(key)){errorHandler()};
-    *sel == '1' ? crypt(enc, key, sel) : (*sel == '2' ? crypt(enc, key, sel) : errorHandler());
-    return(0);
-}
-void crypt(char *enc, char *key, char *sel)
-{
-    static int arrSize;
-    arrSize = strlen(enc);
-    char outArr[arrSize];    
-    for(int i = 0; i < arrSize; i++)
+    if(strlen(enc) != strlen(key))
+    {
+        printf("ERROR: Key is incorrect length");
+        return;
+    }
+
+    char outArr[10];
+
+    for(int i = 0; i < 9; i++)
     {
         enc[i] = toupper(enc[i]);
         key[i] = toupper(key[i]);
-        *sel == '1' ? outArr[i] = ((((((int)enc[i]) - 65) + (((int)key[i]) - 65)) % 26) + 65) : ((((int)enc[i]) - 65) - (((int)key[i]) - 65) < 0 ? outArr[i] = (char)((26 - abs((((int)enc[i]) - 65) - (((int)key[i]) - 65))) + 65) : outArr[i] = (char)(((((int)enc[i]) - 65) - (((int)key[i]) - 65) % 26) + 65));
+
+        int tmp = (((int)enc[i]) - 65) - (((int)key[i]) - 65);
+        if(tmp < 0)
+            outArr[i] = (char)((26 - abs(tmp)) + 65);
+        else
+            outArr[i] = (char)((tmp % 26) + 65);
     }
-    for(int i = 0; i < arrSize; i++){putchar(outArr[i])};
-    putchar('\n');
+
+    printf("Nice one!\nFlag: ");
+    for(int i = 0; i < 9; i++)
+        putchar(outArr[i])
+    putchar('\n')
+
     return;
-}
-void errorHandler()
-{
-        puts("ARGUMENT ERROR");
-        exit(1);
 }
